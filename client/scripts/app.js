b96ba9d4863118922639a8255c5ff660d0fb4d3d
richHong <rich.hong@gmail.com>
2016-03-25 16:38:47 -0700

    Now passing most tests with the exception of addFriend and sendMessage.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -29,8 +29,8 @@ var app = {
       contentType: 'application/json',
       success: function(data){
         console.log('chatterbox: Fetch recieved');
-        objectArr.push(data);
-
+        objectArr.push(data.results);
+        // console.log(objectArr)
       },
       error: function(data){
         console.error('chatterbox: Failed to fetch message')
@@ -39,18 +39,70 @@ var app = {
   },
   //create a server method that points to the server
   server: 'https://api.parse.com/1/classes/chatterbox',
+
+  clearMessages: function(){
+    $('#chats').text('');
+  },
+
+  addMessage: function(message){
+    $('#chats').append('<div>'+message+'</div>');
+  },
+
+  addRoom: function(room){
+    $('#roomSelect').append('<div>'+room+'</div>')
+  },
+
+  addFriend: function(friend){
+    console.log('I tried to')
+    
+  },
+
 };
 var message = {
   username: 'anonymous',
   text: 'testing',
   roomname: 'tga5'
 };
-app.init(message);
+// app.init(message);
+
 $(document).ready(function(){
-_.each(objectArr, function(object){
 
-  $('#posts').append('<div>'+ object.text + '</div>');
+    $('.username').on('click', function(){
+        app.addFriend(this);
+    })
+  
+})
 
-});
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
