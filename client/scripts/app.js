67750f5dd531de2499a05dcf9b623b96a87b3322
richHong <rich.hong@gmail.com>
2016-03-25 12:34:38 -0700

    Initial implementation of config.js and app.js. Passes first 2 spec tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,33 @@
-// YOUR CODE HERE:
+var app = {
+  init: function(){},
+  send: function(){ 
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data){
+        console.log('chatterbox: Message sent');
+      },
+      error: function(data){
+        console.error('chatterbox: Failed to send message')
+      }
+    });
+  },
+  fetch: function(){
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data){
+        console.log('chatterbox: Message sent');
+      },
+      error: function(data){
+        console.error('chatterbox: Failed to send message')
+      }
+    });
+  },
+};
 
+app.message.username('tsdfhsdfk');
