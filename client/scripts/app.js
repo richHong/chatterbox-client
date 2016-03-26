fd4304abbcacf4bc2962551217f0ae60ea58571a
richHong <rich.hong@gmail.com>
2016-03-26 12:24:45 -0700

    Added message to the chat div, prevented XSS attacks, and added Dropdown menu. Currently revising the filtering of the dropdown.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,13 @@
-var objectArr = [];
-var app = {
 
+var app = {
+  //Initialize the app
   init: function(){
-    
+    //Get information from server
     app.fetch()
+
     
   },
+  //Sends message to server
   send: function(message){ 
     $.ajax({
       url: 'https://api.parse.com/1/classes/chatterbox',
@@ -20,6 +22,7 @@ var app = {
       }
     });
   },
+  //Makes ajax call to fetch data from server
   fetch: function(){
     $.ajax({
       //reference server
@@ -28,35 +31,47 @@ var app = {
       contentType: 'application/json',
       success: function(data){
         console.log('chatterbox: Fetch recieved');
-        objectArr.push(data.results);
-        // console.log(objectArr)
+        //Iterate through the result of the data
+        _.each(data.results, function(obj){
+          console.log('obj:',obj);
+          //After recieving data add message to DOM
+          app.addMessage(obj); 
+          //After recieving data add room name to DOM dropdown menu
+          app.addRoom(obj.roomname);
+        })
+      
       },
       error: function(data){
         console.error('chatterbox: Failed to fetch message')
       },
-      complete: function(data){
-        $('#chats').append(data);
-        console.log('data',data)
-      }
+    
     });
   },
   //create a server method that points to the server
   server: 'https://api.parse.com/1/classes/chatterbox',
-
+  //Useful for repopulating chat div
   clearMessages: function(){
     $('#chats').text('');
   },
-
+  //Add message to chat div on DOM
   addMessage: function(message){
-    var msg = $('<div class="username">'+ message.text +'</div>')
+    //create a variable for new divs to prevent XSS attacks
+    var msg = $('<div class="username"/>')
+    msg.text(message.username + ' : '+ message.text);
+    //Click handler add to username so you can add friend
     $('.username').on('click', function(){
+        //adds a friend
         app.addFriend(this);
     })
+    //appends the messages to the #chats div
     $('#chats').append(msg);
   },
 
   addRoom: function(room){
-    $('#roomSelect').append('<div>'+room+'</div>')
+    
+    if($('#roomSelect').find('option').val() !== room){
+      $('#roomSelect').append('<option value="'+room+'">'+room+'</option>')
+    }
   },
 
   addFriend: function(friend){
@@ -75,12 +90,20 @@ var app = {
 
 };
 
+app.init();
 
 $(document).ready(function(){
+  //click element that handles what to do when something is submitted
     $('#send .submit').submit(function(e){
-      app.handleSubmit();
       e.preventDefault();
+      app.handleSubmit();
     });
+    //filters the messages to whatever room is selected
+    $('#roomSelect').change(function(){
+      console.log($('.username').filter($('value').val()));
+  
+    })
+
   
 })
 
